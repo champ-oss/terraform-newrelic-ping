a52a6f55175594f8c0c2d1ccff4d1d1d77a38919
Justin Reeves <reeves122@gmail.com>
2022-06-15 15:30:12 -0500

    create alerting resources for email and slack

--- variables.tf
+++ variables.tf
@@ -53,3 +53,51 @@ variable "status" {
     error_message = "Expected status to be one of [enabled muted disabled]."
   }
 }
+
+variable "runbook_url" {
+  description = "https://registry.terraform.io/providers/newrelic/newrelic/latest/docs/resources/synthetics_alert_condition#runbook_url"
+  type        = string
+  default     = ""
+}
+
+variable "incident_preference" {
+  description = "https://registry.terraform.io/providers/newrelic/newrelic/latest/docs/resources/alert_policy#incident_preference"
+  type        = string
+  default     = "PER_POLICY"
+}
+
+variable "enable_email" {
+  description = "https://registry.terraform.io/providers/newrelic/newrelic/latest/docs/resources/alert_channel#email"
+  type        = bool
+  default     = false
+}
+
+variable "email_recipients" {
+  description = "https://registry.terraform.io/providers/newrelic/newrelic/latest/docs/resources/alert_channel#recipients"
+  type        = string
+  default     = ""
+}
+
+variable "email_include_json_attachment" {
+  description = "https://registry.terraform.io/providers/newrelic/newrelic/latest/docs/resources/alert_channel#include_json_attachment"
+  type        = bool
+  default     = false
+}
+
+variable "enable_slack" {
+  description = "https://registry.terraform.io/providers/newrelic/newrelic/latest/docs/resources/alert_channel#slack"
+  type        = bool
+  default     = false
+}
+
+variable "slack_channel" {
+  description = "https://registry.terraform.io/providers/newrelic/newrelic/latest/docs/resources/alert_channel#channel"
+  type        = string
+  default     = ""
+}
+
+variable "slack_url" {
+  description = "https://registry.terraform.io/providers/newrelic/newrelic/latest/docs/resources/alert_channel#url"
+  type        = string
+  default     = ""
+}
