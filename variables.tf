defb32018eb9b9d31d0f1570ea22668bf15762aa
Justin Reeves <reeves122@gmail.com>
2022-10-05 13:37:29 -0500

    remove unused variables

--- variables.tf
+++ variables.tf
@@ -54,50 +54,8 @@ variable "status" {
   }
 }
 
-variable "runbook_url" {
-  description = "https://registry.terraform.io/providers/newrelic/newrelic/latest/docs/resources/synthetics_alert_condition#runbook_url"
-  type        = string
-  default     = ""
-}
-
-variable "incident_preference" {
-  description = "https://registry.terraform.io/providers/newrelic/newrelic/latest/docs/resources/alert_policy#incident_preference"
-  type        = string
-  default     = "PER_POLICY"
-}
-
-variable "enable_alerts" {
-  description = "https://registry.terraform.io/providers/newrelic/newrelic/latest/docs/resources/alert_channel#email"
-  type        = bool
-  default     = false
-}
-
 variable "enable_synthetic_monitor" {
   description = "enable or disable resource for synthetic monitor"
   type        = bool
   default     = true
-}
-
-variable "email_recipients" {
-  description = "https://registry.terraform.io/providers/newrelic/newrelic/latest/docs/resources/alert_channel#recipients"
-  type        = string
-  default     = ","
-}
-
-variable "email_include_json_attachment" {
-  description = "https://registry.terraform.io/providers/newrelic/newrelic/latest/docs/resources/alert_channel#include_json_attachment"
-  type        = bool
-  default     = false
-}
-
-variable "slack_channel" {
-  description = "https://registry.terraform.io/providers/newrelic/newrelic/latest/docs/resources/alert_channel#channel"
-  type        = string
-  default     = ""
-}
-
-variable "slack_url" {
-  description = "https://registry.terraform.io/providers/newrelic/newrelic/latest/docs/resources/alert_channel#url"
-  type        = string
-  default     = "https://hooks.slack.com/services/XXXXXXX/XXXXXXX/XXXXXXXXXX"
 }
